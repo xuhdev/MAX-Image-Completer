d8ffd5bb7f1a9914c274017fdf76cfea6766a91e
Saishruthi <saishruthi.tn@gmail.com>
2019-02-28 20:21:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from maxfw.core import MAXApp
 from api import ModelMetadataAPI, ModelPredictAPI
 
+
 max_app = MAXApp()
 max_app.add_api(ModelMetadataAPI, '/metadata')
 max_app.add_api(ModelPredictAPI, '/predict')
