09bf29c0bcda353dd95dd8d06c2870b8c4c02b1d
Saishruthi <saishruthi.tn@ibm.com>
2019-03-05 15:56:06 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from maxfw.core import MAXApp
-from api import ModelMetadataAPI, ModelPredictAPI, ModelLabelsAPI
-from config import API_TITLE, API_DESC
+from api import ModelMetadataAPI, ModelPredictAPI
 
-max_app = MAXApp(title = API_TITLE, desc=API_DESC)
+max_app = MAXApp()
 max_app.add_api(ModelMetadataAPI, '/metadata')
-max_app.add_api(ModelLabelsAPI, '/labels')
 max_app.add_api(ModelPredictAPI, '/predict')
 max_app.run()
